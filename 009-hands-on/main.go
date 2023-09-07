95e720217c2ae8e11fcdc57b2d32db6ee9b33558
Cody Anderson <codyanderson@codys-mbp.lan>
2023-09-07 09:42:51 -0600

    first commit

---
+++ 009-hands-on/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var a int
+
+const b int = 42
+
+func main() {
+	a = 21
+	c := 23
+	fmt.Printf("%d and %d and %d \n", a, b, c)
+}
---
+++ 010-hands-on/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println("This part isn't much different is it")
+}
